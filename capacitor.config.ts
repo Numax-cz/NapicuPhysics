93e2fda36533024635068ca81671c90df72b5e3f
Marcel <numax.cz@protonmail.ch>
2024-10-11 18:50:34 +0200

    Added IOS, update configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'eu.napicu.answer',
   appName: 'NapicuAnswer',
-  webDir: 'www'
+  webDir: 'www',
 };
 
 export default config;
